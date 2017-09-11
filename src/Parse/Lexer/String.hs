524f970e414706e3394d183defaf8e09f6221b8d
Adrien Stalain <astalain@novaquark.com>
2017-09-11 13:28:05 +0200

    Added octal escape sequence

--- src/Parse/Lexer/String.hs
+++ src/Parse/Lexer/String.hs
@@ -18,23 +18,44 @@ hex r0 = do
         let res = chr $ (C.digitToInt x1) * 16 + (C.digitToInt x2)
         return (res, r2, srcinc 4)
 
+oct:: BSL.ByteString -> Maybe (Char, BSL.ByteString, SourcePos -> SourcePos)
+oct r0 = do
+          (x1,r1) <- BSLC.uncons r0
+          unless (C.isOctDigit x1) Nothing
+          (x2,r2) <- BSLC.uncons r1
+          unless (C.isOctDigit x2) Nothing
+          (x3,r3) <- BSLC.uncons r2
+          unless (C.isOctDigit x3) Nothing
+
+          let res = chr $ sum [
+                               (C.digitToInt x1) * 64
+                              ,(C.digitToInt x2) * 8
+                              ,(C.digitToInt x3)
+                              ]
+          return (res, r3, srcinc 4)
+
+
 
 escapeSequence:: BSL.ByteString -> (Either ErrorTokenType (Maybe Char), BSL.ByteString, SourcePos -> SourcePos)
 escapeSequence s = fromMaybe (Left UnfinishedString, s, id) $ escape <$> BSLC.uncons s
                     where escape (x,rest) = case x of
-                                              '"'  -> (Right $ Just '"', rest, srcinc 2)
-                                              '\'' -> (Right $ Just '\'', rest, srcinc 2)
-                                              'a'  -> (Right $ Just '\a', rest, srcinc 2)
-                                              'b'  -> (Right $ Just '\b', rest, srcinc 2)
-                                              'f'  -> (Right $ Just '\f', rest, srcinc 2)
-                                              'n'  -> (Right $ Just '\n', rest, srcinc 2)
-                                              'r'  -> (Right $ Just '\r', rest, srcinc 2)
-                                              't'  -> (Right $ Just '\t', rest, srcinc 2)
-                                              'v'  -> (Right $ Just '\v', rest, srcinc 2)
-                                              'x'  -> (case hex rest of
-                                                            Just (c, r, p) -> (Right $ Just c, r, p)
-                                                            Nothing -> (Left BadEscapeCharacter, s, srcinc 1))
-                                              _    -> (Left BadEscapeCharacter, s, srcinc 1)
+                                              '"'                 -> (Right $ Just '"', rest, srcinc 2)
+                                              '\''                -> (Right $ Just '\'', rest, srcinc 2)
+                                              'a'                 -> (Right $ Just '\a', rest, srcinc 2)
+                                              'b'                 -> (Right $ Just '\b', rest, srcinc 2)
+                                              'f'                 -> (Right $ Just '\f', rest, srcinc 2)
+                                              'n'                 -> (Right $ Just '\n', rest, srcinc 2)
+                                              'r'                 -> (Right $ Just '\r', rest, srcinc 2)
+                                              't'                 -> (Right $ Just '\t', rest, srcinc 2)
+                                              'v'                 -> (Right $ Just '\v', rest, srcinc 2)
+                                              'x'                 -> (case hex rest of
+                                                                           Just (c, r, p) -> (Right $ Just c, r, p)
+                                                                           Nothing -> (Left BadEscapeCharacter, s, srcinc 1))
+                                              _ | C.isOctDigit x  -> (case oct s of
+                                                                           Just (c, r, p) -> (Right $ Just c, r, p)
+                                                                           Nothing -> (Left BadEscapeCharacter, s, srcinc 1))
+
+                                              _                   -> (Left BadEscapeCharacter, s, srcinc 1)
 
 
 
