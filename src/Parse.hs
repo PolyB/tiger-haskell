8bd0ff18b65fc57c70411699f84ebeb60c2ad6ed
Adrien Stalain <astalain@novaquark.com>
2017-09-13 13:37:39 +0200

    Adding parsing rules

--- src/Parse.hs
+++ src/Parse.hs
@@ -1,28 +1,26 @@
 {-# LANGUAGE PostfixOperators #-}
+{-# OPTIONS_GHC -fno-warn-missing-signatures #-} 
 
 module Parse (parser) where
 
 import Control.Monad (msum)
 import Data.Functor ((<$), ($>))
-import Data.Functor.Identity
 import Data.Maybe (Maybe(Just, Nothing))
 import Parse.TParser
 import Parse.Tokens
 import Parse.Tokens.Instance ((&))
 import Prelude ((>>), Int, show, String, (.), map, ($))
-import Text.Parsec (token, sepBy, optionMaybe, try)
+import Text.Parsec (token, sepBy, optionMaybe, (<?>), sepBy1, many1)
 import Text.Parsec.Combinator (eof)
 import Text.Parsec.Expr
+import Data.ByteString as BS (ByteString)
 
 -- TODO output the AST
-parser :: TParser ()
 parser = exp >> eof
 
 
-exps :: TParser ()
 exps = () <$ exp `sepBy` (T_Semicolon&)
 
-exp :: TParser ()
 exp = buildExpressionParser optable $ msum [
           () <$ ( integer )
         , () <$ ( string )
@@ -33,13 +31,28 @@ exp = buildExpressionParser optable $ msum [
 
         , () <$ ( (T_If&) >> exp >> (T_Then&) >> exp >> optionMaybe ( (T_Else&) >> exp) )
         , () <$ ( (T_While&) >> exp >> (T_Do&) >> exp )
+        , () <$ ( (T_Let&) >> decs >> (T_In&) >> exps >> (T_End&))
         , () <$ ( (T_Break&) )
     ]
 
-nopeop :: () -> () -> ()
+decs = many1 dec
+
+dec = msum [
+          () <$ ( (T_Import&) >> string ),
+          () <$ ( (T_Type&) >> identifier >> (T_Equal&) >> ty)
+          ]
+          
+
+ty  = msum [
+          type_id,
+          ((T_Array&) >> (T_Of&) >> type_id)
+        ]
+
+tyfields = (<$) () (identifier >> (T_Colon&) >> type_id) `sepBy1` (T_Comma&)
+
+type_id = () <$ identifier
 nopeop _ _ = ()
 
-optable :: OperatorTable [PosToken] () Identity ()
 optable = (map . map) (\(x,y)-> Infix y x) [
             [
               (AssocLeft, (T_Mult&) $> nopeop ),
@@ -63,13 +76,19 @@ optable = (map . map) (\(x,y)-> Infix y x) [
 
 
 string :: TParser String
-string = token (\(PosToken _ t) -> show t)  pos test
+string = token (\(PosToken _ t) -> show t)  pos test <?> "string"
           where pos (PosToken p _)             = p
                 test (PosToken _ (T_String x)) = Just x
                 test _                         = Nothing
 
 integer :: TParser Int
-integer = token (\(PosToken _ t) -> show t) pos test
+integer = token (\(PosToken _ t) -> show t) pos test <?> "integer"
             where pos   (PosToken p _)          = p
                   test  (PosToken _ (T_Int x))  = Just x
+                  test _                        = Nothing
+
+identifier :: TParser BS.ByteString
+identifier = token (\(PosToken _ t) -> show t) pos test
+            where pos   (PosToken p _)          = p
+                  test  (PosToken _ (T_Id x))   = Just x
                   test _                        = Nothing
