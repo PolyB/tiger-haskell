7c6b33671c57ae083b0c5ed3987fa5f31a0a86cf
Adrien Stalain <adrien.stalain@epita.fr>
2017-09-16 02:04:27 +0200

    refixed postfix parsing

--- src/Parse/PostFix.hs
+++ src/Parse/PostFix.hs
@@ -3,13 +3,14 @@ module Parse.PostFix (postfix) where
 import Parse.TParser
 import Text.Parsec
 import Control.Monad
+import Debug.Trace
 
 
 
 postfix:: [TParser (a->a)] -> TParser a -> TParser a
 postfix f base = try $ do
                       x <- base
-                      c <- msum $ (try . optionMaybe) <$> f
+                      c <- optionMaybe $ msum $ (try <$> f)
                       case c of
                         Nothing -> return x
                         Just fun  -> postfix f (return $ fun x)
